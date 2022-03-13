b21898fc0ae9ea053b031d14cacefb658fcc5862
Pixwl <pixwl@pixwl.dev>
2022-03-14 09:19:36 +1030

    fix(Globals.java): Make class public

--- src/main/java/cc/hayhay/playerdeaths/Globals.java
+++ src/main/java/cc/hayhay/playerdeaths/Globals.java
@@ -4,7 +4,7 @@ import org.bukkit.entity.Player;
 
 import java.util.*;
 
-class Globals {
+public class Globals {
     public static HashMap<Player, Double> playerEventList = new HashMap<>();
     public static HashMap<Player, ArrayList<String>> playerDeaths = new HashMap<>();
     public static ArrayList<String> deathTypes = new ArrayList<>();
